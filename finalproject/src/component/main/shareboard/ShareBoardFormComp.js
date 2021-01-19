cb8b5e8019a3e9b1458f7e95b88df7ab0b1d8378
yanghojun <twinhj@naver.com>
2021-01-19 16:43:25 +0900

    shareboardform 데이터 db넣기 시도중

--- finalproject/src/component/main/shareboard/ShareBoardFormComp.js
+++ finalproject/src/component/main/shareboard/ShareBoardFormComp.js
@@ -12,13 +12,13 @@ class ShareBoardFormComp extends Component {
       photoname:''
      }
 
-    //서버에 임지 업로드하는 함수
+    //서버에 이미지 업로드하는 함수
     uploadImage=(e)=>{
       const uploadFile=e.target.files[0];
       const imageFile=new FormData();
       imageFile.append("uploadFile",uploadFile);
  
-      let url="http://192.168.0.220:9002/share/list?start=0&perPage=3";
+      let url="http://192.168.0.220:9002/share/insert";
       
       axios({
           method:'post',
@@ -33,36 +33,44 @@ class ShareBoardFormComp extends Component {
 
       }
 
-      onDataInsert=()=>{
-         //입력값 state 변수에 저장하기
-         let subject=this.refs.subject.value;
-         let addr=this.refs.addr.value;
-         let content=this.refs.content.value;
+       onDataInsert=()=>{
+          //입력값 state 변수에 저장하기
+          let subject=this.refs.subject.value;
+          let addr=this.refs.addr.value;
+          let content=this.refs.content.value;
          
          
-         //db 에 insert
-         let url="http://192.168.0.220:9002/share/list?start=0&perPage=3";
-         axios.post(url,{subject,addr,content})
-         .then(res=>{
-             //값 지우기
-             this.refs.subject.value='';
-             this.refs.addr.value='';
-             this.refs.content.value='';
+          //db 에 insert
+          let url="http://192.168.0.220:9002/share/insert";
+          axios.post(url,{subject,addr,content})
+          .then(res=>{
+              //값 지우기
+              this.refs.subject.value='';
+              this.refs.addr.value='';
+              this.refs.content.value='';
              
             
-             //이미지도 지우기
-             this.setState({
-                 photoname:''
-             })
-         })
+              //이미지도 지우기
+              this.setState({
+                  photoname:''
+              })
+          })
  
-      }
+       }
 
-      componentWillMount()
-      {
-         this.list();
-      }
- 
+      handleChange(event) {
+         this.setState({value: event.target.subject});
+         this.setState({value: event.target.addr});
+         this.setState({value: event.target.content});
+       }
+     
+       handleSubmit(event) {
+         alert('공유하였습니다. 목록확인하세요 ' + this.state.subject);
+         event.preventDefault();
+         this.onDataInsert();
+       }
+
+
 
 
 
@@ -70,31 +78,33 @@ class ShareBoardFormComp extends Component {
         super(props);
         console.log("ShareBoardFormComp constructor", props);
 
-     
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
  
     }
 
   
 
     render() {
-        const url="http://192.168.0.220:9002/share/list?start=0&perPage=3";
+        const url="http://192.168.0.220:9002/share/insert";
         console.log("ShareBoardFormComp render()", this.props);
         return (
             <div>
                 <b>맛집공유</b>
 
+                <form onSubmit={this.handleSubmit} method="post">
                 <table style={{width:'800px',border:'1px solid black'}}>
                    <tr>
                       <th><span>맛집이름</span></th>
                       <td>
-                         <input type="text" style={{width:'200px',height:'20px'}} placeholder="맛집이름을 적어주세요"/>
+                         <input type="text" style={{width:'200px',height:'20px'}} placeholder="맛집이름을 적어주세요" name="subject" />
                       </td>
                    </tr>
 
                    <tr>
                       <th><span>맛집주소</span></th>
                       <td>
-                         <input type="text" style={{width:'400px',height:'20px'}} placeholder="맛집주소를 적어주세요"/>
+                         <input type="text" style={{width:'400px',height:'20px'}} placeholder="맛집주소를 적어주세요" name="addr"/>
                       </td>
                    </tr>
 
@@ -109,7 +119,7 @@ class ShareBoardFormComp extends Component {
                    <tr>
                       <th><span>리뷰</span></th>
                       <td>
-                         <textarea maxLength="1200" style={{width:'400px',height:'120px',resize:'none'}} ref="content" ></textarea>
+                         <textarea maxLength="1200" style={{width:'400px',height:'120px',resize:'none'}} name="content" ></textarea>
                       </td>
                    </tr>
 
@@ -121,16 +131,18 @@ class ShareBoardFormComp extends Component {
                    </tr>
 
                 </table>
+               
 
                 <div>
                     
-                    <button type="button" onClick={this.onDataInsert.bind(this)}>공유하기</button>
+                    <input type="submit" value="공유하기"/>
                     <Link to="./ShareBoard/ShareBoardPageComp">
                     <button type="button">맛집목록</button> 
                     </Link>
                     <Route exact path="/ShareBoard/ShareBoardPageComp" component={ShareBoardPageComp}/>
                 </div>
 
+                </form>
             </div>
         )
     }
