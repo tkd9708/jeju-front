c89c3a25d5bac3bb99b8b64667c3ccde5e0e1c37
sykim86 <sykim8611@gmail.com>
2021-01-19 10:47:51 +0900

    액션타입 config에서 UPPER_SNAKE_CASE로 바꿨습니다

--- finalproject/src/redux/config.js
+++ finalproject/src/redux/config.js
@@ -1,14 +1,14 @@
 export const actionType = {
     setMainView: "setMainView",
     setSpotView: "setSpotView",
-    Singup: "Signup",
-    login: "login",
-    loginRequest: "loginRequest",
-    loginSuccess: "loginSuccess",
-    loginFailure: "loginFailure",
-    memberList: "memberList", // 회원 리스트 요청 시작
-    mbListS: "mbListS", // 회원 리스트 요청 성공
-    mbListF: "mbListF", // 회원 리스트 요청 실패
+    SIGN_UP: "SIGN_UP",
+    LOG_IN: "LOG_IN",
+    LOGIN_REQUEST: "LOGIN_REQUEST",
+    LOGIN_SUCCESS: "LOGIN_SUCCESS",
+    LOGIN_FAILURE: "LOGIN_FAILURE",
+    MEMBER_LIST: "MEMBER_LIST", // 회원 리스트 요청 시작
+    MB_LIST_S: "MB_LIST_S", // 회원 리스트 요청 성공
+    MB_LIST_F: "MB_LIST_F", // 회원 리스트 요청 실패
 }
 
 export const mainViewType = {
