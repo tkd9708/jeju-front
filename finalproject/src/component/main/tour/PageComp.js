2ca8e0e8f3db12894eccd14fd54a26070d9b8c0a
tkd9708 <tkd9708@naver.com>
2021-01-19 17:13:58 +0900

    tour 페이징처리

---
+++ finalproject/src/component/main/tour/PageComp.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react';
+
+class PageComp extends Component {
+
+    render() {
+        const{startPage, endPage, currentPage, totalPage, paginate} = this.props;
+
+        let prev = startPage>1?<li class="page-item"><a class="page-link" onClick={(e)=>{
+            e.preventDefault();
+            this.props.paginate(startPage - 1);
+        }}>◀</a></li>:"";
+
+        let next = endPage < totalPage?<li class="page-item"><a class="page-link" onClick={(e)=>{
+            e.preventDefault();
+            this.props.paginate(endPage + 1);
+        }}>▶</a></li>:"";
+
+        let page = [];
+        for(let i=startPage; i<=endPage; i++){
+            page.push(i);
+        }
+        let pages = page.map(function(num,idx){
+            return <li class="page-item"><a class="page-link" style={{color : num==currentPage?"red":"black"}} onClick={(e)=>{
+                e.preventDefault();
+                paginate(num);
+            }}>{num}</a></li>
+        });
+
+        return (
+            <div>
+                {startPage},{endPage},{totalPage}
+                <ul class="pagination">
+                    {prev}
+                    {pages}
+                    {next}
+			    </ul>
+            </div>
+        );
+    }
+}
+
+export default PageComp;
--- finalproject/src/component/main/tour/TourPageComp.js
+++ finalproject/src/component/main/tour/TourPageComp.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
 import store from "../../../redux/store";
+import ItemComp from "./ItemComp";
+import axios from "axios";
+import PageComp from "./PageComp";
 
 class TourPageComp extends Component {
 
     state = {
-        area : store.getState().spotView
+        area : store.getState().spotView,
+        spotList : [],
+        pageNum : '0'
     }
 
     constructor(props) {
@@ -17,13 +22,85 @@ class TourPageComp extends Component {
             });
         }.bind(this));
 
+        this.currentPage=1;
+        this.totalCount=0;
+        this.perPage = 12; // 한페이지당 보여질 글의 갯수
+        this.perBlock = 5; // 한블럭당 출력할 페이지의 갯수
+        this.totalPage=0; // 총 페이지의 갯수
+        this.startPage=0; // 각 블럭당 시작 페이지 번호
+        this.endPage=0; // 각 블럭당 끝페이지 번호
+        this.start=0; // 각 블럭당 불러올 글의 시작번호
+        this.end=0; // 각 블럭당 글의 끝번호
+        this.no=0; // 각 페이지에서 출력할 시작번호
+    }
+
+    getList=()=> {
+
+        // 나머지가 있을 경우에는 1페이지 더 추가 (예 : 총글수가 9이고 한페이지당 2개씩 볼 경우)
+        this.totalPage = Math.floor(this.totalCount/this.perPage) + (this.totalCount % this.perPage > 0 ? 1 : 0);
+        
+        // 시작페이지와 끝페이지 구하기
+        this.startPage = Math.floor((this.currentPage - 1) / this.perBlock) * this.perBlock + 1;
+        this.endPage = this.startPage + this.perBlock - 1;
+        // 마지막 블럭은 endPage를 totalPage로 해놔야 한다.
+        if(this.endPage > this.totalPage)
+            this.endPage = this.totalPage;
+        
+        // mysql은 첫 글이 0번이므로 +1 안해도됨 (오라클은 1번)
+        this.start = (this.currentPage-1) * this.perPage;
+        
+        this.no = this.totalCount-(this.currentPage - 1) * this.perPage;
+
+        let url = "http://localhost:9002/spot/list?start=" + this.start + "&perPage=" + this.perPage + "&label2=" + this.state.area;
+
+        axios.get(url)
+            .then(res=>{
+                this.setState({
+                    spotList: res.data
+                })
+            }).catch(err=>{
+                console.log("spotlist getList 오류 : " + err);
+            })
+    }
+
+    getTotalCount=()=>{
+        let url = "http://localhost:9002/spot/count?label2=" + this.state.area;
+
+        axios.get(url)
+            .then(res=>{
+                this.totalCount = res.data;
+                this.getList();
+            }).catch(err=>{
+                console.log("spotlist getTotalCount 오류 : " + err);
+            })
+    }
+
+    componentWillMount(){
+        this.getTotalCount();
+        
+    }
+
+    paginate = (num) => {
+        
+        this.currentPage = num;
+        this.getList();
     }
 
     render() {
         console.log("TourPageComp render()", this.props);
         return (
             <div>
-                <h4>TourPageComp {this.state.area}</h4>
+                <h4>TourPageComp {this.state.area} {this.totalCount}</h4>
+                {/* 이미지 넣기 */}
+
+                {/* list 출력 */}
+                {this.state.spotList.map((row,idx)=>(
+                    <ItemComp row={row} key={idx} history={this.props.history}></ItemComp>
+                ))}
+
+                {/* 페이징 */}
+                <PageComp area={this.state.area} startPage={this.startPage} endPage={this.endPage} currentPage={this.currentPage} 
+                    totalPage={this.totalPage} paginate={this.paginate.bind(this)}></PageComp>
             </div>
         )
     }
