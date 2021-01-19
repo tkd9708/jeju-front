15d03e37edb4cccc0ad7c387064112f3881079aa
sykim86 <sykim8611@gmail.com>
2021-01-19 14:56:49 +0900

    config파일 userDispatch()를 없앳습니다

--- finalproject/src/redux/config.js
+++ finalproject/src/redux/config.js
@@ -16,18 +16,6 @@ export const actionType = {
     MB_LIST_F: "MB_LIST_F", // 회원 리스트 요청 실패
 }
 
-// useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할수있게 해주는 Hook이다
-const dispatch = useDispatch();
-// 각 액션을 dispatch 하는 함수
-
-
-// export const memberList = () => async dispatch => {
-//     dispatch({ type: MEMBER_LIST }); // 요청이 시작됨
-//     try {
-//         const 
-//     }
-// }
-
 export const mainViewType = {
     MainPage: "mainPage",
     Home: "home",
