e32e894bc5e94b02c0b7c26a259a454853d27c1f
ykh <yangyk7364@gmail.com>
2021-01-22 08:16:27 +0900

    main page 작업 photo scroll

--- finalproject/src/component/main/mainpage/MainPhotoComp.js
+++ finalproject/src/component/main/mainpage/MainPhotoComp.js
@@ -7,14 +7,63 @@ import gsap from "gsap";
 
 class MainPhotoComp extends Component {
 
+
     constructor(props) {
         super(props);
 
-
+        // this.setImageScroll();
     }
 
-    setImageScroll = () => {
-        gsap.to("")
+    setImageScroll = (photoNum, preNum = 4) => {
+        const duration = 5;
+        const imgCnt = 4;
+        let arrImg = new Array();
+
+        //over index setting
+        if (photoNum > imgCnt) {
+            photoNum = 1;
+        }
+
+        //img element array setting.
+        for (let i = 1; i <= imgCnt; i++) {
+            let tempImgIdx = (photoNum - 1) + i;
+            tempImgIdx = tempImgIdx > imgCnt ? tempImgIdx - imgCnt : tempImgIdx;
+            arrImg.push(document.querySelector(`img.mainPhotoComp_img${tempImgIdx}`));
+        }
+        console.log("arrImg:", arrImg);
+        console.log("photoNum", photoNum, "preNum", preNum);
+
+        //zIndex setting.
+        for (let i = 0; i < imgCnt; i++) {
+            arrImg[i].style.zIndex = imgCnt - i;
+
+        }
+
+        // document.querySelector(`img.mainPhotoComp_img${preNum}`).style.opacity = 0;
+
+
+        //tween action.
+        gsap.fromTo(`.mainPhotoComp_img${photoNum}`
+            , {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: duration,
+            });
+        gsap.fromTo(`.mainPhotoComp_img${preNum}`
+            , {
+                opacity: 1,
+            }, {
+                opacity: 0,
+                duration: duration,
+            });
+
+        //next action preparing.
+        setTimeout(this.setImageScroll.bind(this, photoNum + 1, photoNum), (duration + 3) * 1000);
+    }
+
+    componentDidMount() {
+        this.setImageScroll(1);
     }
 
     render() {
@@ -22,7 +71,7 @@ class MainPhotoComp extends Component {
             <div className="mainPhotoComp">
                 <img className="mainPhotoComp_img1" src={jeju1}/>
                 <img className="mainPhotoComp_img2" src={jeju2}/>
-                <img className="mainPhotoComp_img3" style={{width:"1920px"}} src={jeju3}/>
+                <img className="mainPhotoComp_img3" style={{width: "1920px"}} src={jeju3}/>
                 <img className="mainPhotoComp_img4" src={udo1}/>
             </div>
         );
