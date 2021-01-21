8480dae908adc8c5b8907fbc4d6d46b6aaf64d3f
ykh <yangyk7364@gmail.com>
2021-01-21 17:18:07 +0900

    main page 작업

--- finalproject/src/component/main/mainpage/MainPhotoComp.js
+++ finalproject/src/component/main/mainpage/MainPhotoComp.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 import jeju1 from "../../../image/jeju1.jpg";
 import jeju2 from "../../../image/jeju2.jpg";
+import jeju3 from "../../../image/jeju3.jpeg";
 import udo1 from "../../../image/udo1.jpg";
+import gsap from "gsap";
 
 class MainPhotoComp extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    setImageScroll = () => {
+        gsap.to("")
+    }
+
     render() {
         return (
             <div className="mainPhotoComp">
-                <img src={jeju1}/>
-                <img src={jeju2}/>
-                <img src={udo1}/>
+                <img className="mainPhotoComp_img1" src={jeju1}/>
+                <img className="mainPhotoComp_img2" src={jeju2}/>
+                <img className="mainPhotoComp_img3" style={{width:"1920px"}} src={jeju3}/>
+                <img className="mainPhotoComp_img4" src={udo1}/>
             </div>
         );
     }
