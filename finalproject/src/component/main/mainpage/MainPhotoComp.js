9ecb2e754d11b707ed09703f7bae6ac156cdbf56
sykim86 <sykim8611@gmail.com>
2021-01-22 08:34:12 +0900

    메인페이지

--- finalproject/src/component/main/mainpage/MainPhotoComp.js
+++ finalproject/src/component/main/mainpage/MainPhotoComp.js
@@ -4,9 +4,15 @@ import jeju2 from "../../../image/jeju2.jpg";
 import jeju3 from "../../../image/jeju3.jpeg";
 import udo1 from "../../../image/udo1.jpg";
 import gsap from "gsap";
+import WaveBorder from "./WaveBorder";
+import { createMuiTheme, MuiThemeProvider, WithStyles } from "@material-ui/core";
+import { ThemeProvider } from 'styled-components';
+import theme from "./theme";
+import PropTypes from "prop-types";
+import styledComponents from "styled-components";
 
 class MainPhotoComp extends Component {
-
+    
     constructor(props) {
         super(props);
 
@@ -19,12 +25,21 @@ class MainPhotoComp extends Component {
 
     render() {
         return (
-            <div className="mainPhotoComp">
-                <img className="mainPhotoComp_img1" src={jeju1}/>
-                <img className="mainPhotoComp_img2" src={jeju2}/>
-                <img className="mainPhotoComp_img3" style={{width:"1920px"}} src={jeju3}/>
-                <img className="mainPhotoComp_img4" src={udo1}/>
-            </div>
+            <>
+                <div className="mainPhotoComp">
+                    <img className="mainPhotoComp_img1" src={jeju1}/>
+                    <img className="mainPhotoComp_img2" src={jeju2}/>
+                    <img className="mainPhotoComp_img3" style={{width:"1920px"}} src={jeju3}/>
+                    <img className="mainPhotoComp_img4" src={udo1}/>
+                </div>
+                <MuiThemeProvider theme={theme}>
+                    <WaveBorder 
+                        upperColor="#FFFFFF"
+                        lowerColor={theme.palette.common.darkBlack}
+                        animationNegativeDelay={4}
+                    />
+                </MuiThemeProvider>
+            </>
         );
     }
 }
