d5dac9971c1b0245667f4ef956d025d80accde54
ykh <yangyk7364@gmail.com>
2021-01-21 14:02:49 +0900

    MainPageComp 기본작업 구성

--- finalproject/src/component/main/mainpage/MainPageComp.js
+++ finalproject/src/component/main/mainpage/MainPageComp.js
@@ -6,6 +6,9 @@ import ShareBoardMiniComp from "./ShareBoardMiniComp";
 import axios from "axios";
 import {URL} from "../../../redux/config";
 import "./MainPageComp.css"
+import MainPhotoComp from "./MainPhotoComp";
+import MyPlanComp from "./MyPlanComp";
+import EtcBoardComp from "./EtcBoardComp";
 
 class MainPageComp extends Component {
 
@@ -70,16 +73,13 @@ class MainPageComp extends Component {
         return (
             <div className="mainPageComp">
                 <h4>main page comp</h4>
-                <SearchComp name={this.state.search}/>
-                <HotPlaceComp ace name={this.state.viewspot}
-                          setMainView={this.props.setMainView}
-                />
-                <div className="miniTable">
-                    <NoticeMiniComp list={this.state.noticeList}/>
-                    <ShareBoardMiniComp list={this.state.shareBoardList}/>
+                <MainPhotoComp/>
+                <div className="search-myplan">
+                    <SearchComp/>
+                    <MyPlanComp/>
                 </div>
-
-
+                <HotPlaceComp/>
+                <EtcBoardComp/>
             </div>
         )
     }
