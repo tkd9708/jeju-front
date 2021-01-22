df9b176c16e19f4dff512ad3e8ecd7a3e6eae1c0
sanggeun-oh <rjfjqj03@naver.com>
2021-01-22 21:17:07 +0900

    스케줄 추가_오상근

---
+++ finalproject/src/component/main/mypage/MySchedule.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react'
+import './style/RCA.css';
+import moment from 'moment';
+import Header from './Header';
+import Calendar from './Calendar';
+
+
+class MySchedule extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log("MySchedule constructor", props);
+
+    }
+      state={
+        calendarYM : moment(),
+        today : moment(),
+        selected : moment().format("YYYY-MM-DD")
+
+      }
+
+      static defaultProps = {
+        clickFn : ()=>{}
+    }
+
+    moveMonth = (month) => {
+        this.setState({
+            calendarYM : this.state.calendarYM.add(month,'M')
+        })
+    }
+
+    changeSelected = (clickedDate) =>{
+
+        if(moment(clickedDate).isSame(this.state.selected,'day')){
+            this.props.clickFn(clickedDate);
+            return;
+        }
+
+        this.setState({
+            selected : clickedDate
+        })
+
+        this.props.clickFn(clickedDate)
+        
+        if(moment(clickedDate).isBefore(this.state.calendarYM,'month')){
+            this.moveMonth(-1)
+        }else if(moment(clickedDate).isAfter(this.state.calendarYM,'month')){
+            this.moveMonth(1)
+        }
+    }
+    render(){
+        return(
+            <div className="test-layout">
+               <div className="RCA-app-container">
+               
+                <Header calendarYM={this.state.calendarYM.format("YYYY년 MM월")}
+                        today={this.state.today.format("현재: YYYY - MM - DD")}
+                        
+                        moveMonth={this.moveMonth}/>
+                    
+                     
+                    <Calendar YM={this.state.calendarYM.format("YYYY-MM-DD")}
+                        selected={this.state.selected}
+                        changeSelected={this.changeSelected}
+                    />
+                </div>
+        )
+    
+    </div>
+        )
+    }
+
+}
+
+export default MySchedule;
