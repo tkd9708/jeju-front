d06069ab32425097a0c535d3e3f9c8e845b28043
sykim86 <sykim8611@gmail.com>
2021-01-22 17:31:33 +0900

    네이버 회원가입 안됩니다

---
+++ finalproject/src/component/main/auth/FaceBookLoginBtnComp.js
@@ -0,0 +1,61 @@
+import FacebookLogin from "react-facebook-login";
+import React, { Component } from "react";
+import styled from "styled-components";
+import { FaFacebookSquare } from "react-icons/fa";
+
+class FaceBookLoginBtnComp extends Component {
+
+    state = {
+        isLoggedIn: false,
+        id: '',
+        name: '',
+        email: '',
+    }
+
+    responseFacebook = response => {
+        console.log(response);
+
+        this.setState({
+            isLoggedIn:true,
+            id: response.id,
+            name: response.name,
+            email: response.email,
+        });
+    };
+
+    componentClicked = () => console.log('clicked');
+
+    render() {
+
+        let fbContent;
+
+        if(this.state.isLoggedIn) {
+            fbContent = (
+                <div>
+                    <h2>{this.state.name}</h2>
+                    Email : {this.state.email}
+                </div>
+            );
+        }
+        else {
+            fbContent = (
+                <FacebookLogin
+                    appId="513629929622627"
+                    autoLoad={true} // 실행과 동시에 자동으로 로그인 팝업창이 뜸
+                    fields="name,email.picture" // 어떤정보를 받아올지 입력하는 필드
+                    scope="public_profile,user_friends,user_actions.books"
+                    onClick={this.componentClicked} // this.바인딩 추가
+                    callback={this.responseFacebook} // this.바인딩 추가
+                />
+            )
+        }
+
+        return (
+            <div>
+                {fbContent}
+            </div>
+        );
+    }   
+}
+
+export default FaceBookLoginBtnComp;
