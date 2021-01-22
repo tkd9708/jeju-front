b3a78949fef16ec7f7b719d204bc6cd47f8f3335
krapoownij <jinwoo1199@naver.com>
2021-01-22 11:10:30 +0900

    Revert "notice 작업중"
    This reverts commit 7fcaa4a15569914448a1f671b1237aa181dfa73d.

--- finalproject/src/component/main/notice/NoticePageComp.js
+++ finalproject/src/component/main/notice/NoticePageComp.js
@@ -1,105 +1,18 @@
 import React, {Component} from "react";
-import PlusImg from "../../../image/plus.png";
-import BoardItem from "./BoardItem";
-import axios from 'axios';
-import './NoticeCss.css';
+
 
 class NoticePageComp extends Component {
-    state={
-        maxNo:7,
-        listData:[]
-        
+
+    constructor(props) {
+        super(props);
+        console.log("NoticePageComp constructor", props);
     }
 
-    list=()=>{
-        let url="http://192.168.0.3:9002/notice/list";
-        console.log(url);
-        axios.get(url)
-        .then(res=>{
-            this.setState({
-                listData:res.data
-            })
-        })
-    }
-
-    // handelSave=(data)=>{
-    //     console.dir(data);
-
-    //     this.setState({
-    //         maxNo:++this.state.maxNo,
-    //         boards:this.state.boards.concat({
-    //             brdno:this.state.maxNo,
-    //             brddate:new Date(),
-    //             brdtitle: data.brdtitle
-    //         })
-    //     })
-    // }
-
-    componentWillMount()
-    {
-        this.list();
-    }
     render() {
-        const {boards}=this.state;
-
-    
+        console.log("NoticePageComp render()", this.props);
         return (
             <div>
-                {/*<a href="#NoticeMiniComp">{this.props.name}</a>*/}
-            
-                <br/>
-                {this.props.name}<br/>
-                <h5>공지사항아아아</h5>
-                <button>
-                    <img src={PlusImg}
-                         style={{width: "50px"}}
-                    />
-                </button>
-                <table style={{width:'400px'}} id="noticeTable">
-                    <caption><b>공지사항</b></caption>
-                    <thead>
-                    <tr style={{backgroundColor:'orange'}}>
-                        <th style={{width:'60px'}}>#</th>
-                        <th style={{width:'200px'}}>제목</th>
-                        <th style={{width:'50px'}}>작성일</th>
-                        <th style={{width:'50px'}}>⭐</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                      this.state.listData.map((row,idx)=>(
-                        <BoardItem row={row} key={idx} idx={idx}
-                        history={this.props.history}/>
-                        ))
-                    }
-                    {/* <tr>
-                        <td>
-                            <a href="#">
-                                <span>1제목 입니다.</span>
-                            </a>
-                        </td>
-                        <td>1작성일 입니다.</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <span>2제목 입니다.</span>
-                            </a>
-                        </td>
-                        <td>2작성일 입니다.</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <span>3제목 입니다.</span>
-                            </a>
-                        </td>
-                        <td>3작성일 입니다.</td>
-                    </tr> */}
-                    </tbody>
-                </table>
-
-
+                <h4>notice page comp</h4>
             </div>
         )
     }
