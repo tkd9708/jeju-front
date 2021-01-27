71656cbf4247ebbd4ea57d4020cac60287cb4655
ykh <yangyk7364@gmail.com>
2021-01-27 21:06:33 +0900

    [redux-persist] 추가

--- finalproject/src/redux/store.js
+++ finalproject/src/redux/store.js
@@ -1,9 +1,7 @@
-import {applyMiddleware, compose, createStore} from "redux";
+import {createStore} from "redux";
 import {actionType, mainViewType} from "./config";
-
-import thunk from "redux-thunk";
-
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+import {persistReducer} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 
 const footerStyle = {
     sizeIn: {
@@ -36,92 +34,98 @@ const setPositionFooter = () => {
             footerComp.style.position = "";
             footerComp.style.bottom = "";
         }
-    } else{
+    } else {
         window.setTimeout(setPositionFooter, 100);
     }
 }
 
+const persistConfig = {
+    key: "root",
+    storage
+}
 
-export default createStore(
-    function (state, action) {
+const reducer = function (state, action) {
 
-        //state 초기화.
-        if (state === undefined) {
-            return {
-                mainView: mainViewType.MainPage,
-                logged: false,
-                memberData: [], // 회원목록
-                pageNum: '1',
-                id: '',
-                loginId: '',
-                googleOn: false,
-                mainSearch: {
-                    category: "all",
-                    searchVal: "",
-                    searchResultDataList: [],
-                },
+    //state 초기화.
+    if (state === undefined) {
+        return {
+            mainView: mainViewType.MainPage,
+            logged: false,
+            memberData: [], // 회원목록
+            pageNum: '1',
+            id: '',
+            loginId: '',
+            googleOn: false,
+            mainSearch: {
+                category: "all",
+                searchVal: "",
+                searchResultDataList: [],
+            },
 
-            }
         }
-
-        // state 불변성 유지해야함. immutability
-        // newState 새로 파서 기존꺼 + 변경할거 넣은다음 redux의 state 바꿔치기.
-        let newState = {};
-
-        //action.type 별로 로직처리 다르게 하기.
-        if (action.type === actionType.setMainView) {
-            window.setTimeout(setPositionFooter, 100);
-            newState = Object.assign({}, state, {
-                mainView: action.mainView,
-            });
-        } else if (action.type === actionType.SIGN_UP) {
-
-            newState = Object.assign({}, state, {
-                mainView: action.mainView,
-            });
-        } else if (action.type === actionType.LOG_IN) {
-            newState = Object.assign({}, state, {
-                loginId: action.loginId
-            });
-        } else if (action.type === actionType.LOGIN_REQUEST) {
-            newState = Object.assign({}, state, {});
-        } else if (action.type === actionType.LOGIN_SUCCESS) {
-            newState = Object.assign({}, state, {
-                logged: true,
-                id: action.type.id,
-            });
-        } else if (action.type === actionType.LOGIN_FAILURE) {
-            newState = Object.assign({}, state, {
-                logged: false,
-                id: "",
-            });
-        } else if (action.type === actionType.MEMBER_LIST) {
-            newState = Object.assign({}, state, {
-                memberData: action.payload
-            });
-        } else if (action.type === actionType.shareBoardUpdate) {
-            newState = Object.assign({}, state);
-        } else if (action.type === actionType.tourPage) {
-            newState = Object.assign({}, state, {
-                pageNum: action.pageNum
-            });
-        } else if (action.type === actionType.googleLogin) {
-            newState = Object.assign({}, state, {
-                googleOn: action.googleOn
-            })
-        } else if (action.type === actionType.setSearchResultDataList) {
-            newState = Object.assign({}, state, {
-                mainSearch: {
-                    category: action.category,
-                    searchVal: action.searchVal,
-                    searchResultDataList: action.searchResultDataList,
-                },
-            });
-        }
-
-        console.log("reducer()", state, action, newState);
-        return newState;
     }
-    // , composeEnhancers(applyMiddleware(thunk))
+
+    // state 불변성 유지해야함. immutability
+    // newState 새로 파서 기존꺼 + 변경할거 넣은다음 redux의 state 바꿔치기.
+    let newState = {};
+
+    //action.type 별로 로직처리 다르게 하기.
+    if (action.type === actionType.setMainView) {
+        window.setTimeout(setPositionFooter, 100);
+        newState = Object.assign({}, state, {
+            mainView: action.mainView,
+        });
+    } else if (action.type === actionType.SIGN_UP) {
+
+        newState = Object.assign({}, state, {
+            mainView: action.mainView,
+        });
+    } else if (action.type === actionType.LOG_IN) {
+        newState = Object.assign({}, state, {
+            loginId: action.loginId
+        });
+    } else if (action.type === actionType.LOGIN_REQUEST) {
+        newState = Object.assign({}, state, {});
+    } else if (action.type === actionType.LOGIN_SUCCESS) {
+        newState = Object.assign({}, state, {
+            logged: true,
+            id: action.type.id,
+        });
+    } else if (action.type === actionType.LOGIN_FAILURE) {
+        newState = Object.assign({}, state, {
+            logged: false,
+            id: "",
+        });
+    } else if (action.type === actionType.MEMBER_LIST) {
+        newState = Object.assign({}, state, {
+            memberData: action.payload
+        });
+    } else if (action.type === actionType.shareBoardUpdate) {
+        newState = Object.assign({}, state);
+    } else if (action.type === actionType.tourPage) {
+        newState = Object.assign({}, state, {
+            pageNum: action.pageNum
+        });
+    } else if (action.type === actionType.googleLogin) {
+        newState = Object.assign({}, state, {
+            googleOn: action.googleOn
+        })
+    } else if (action.type === actionType.setSearchResultDataList) {
+        newState = Object.assign({}, state, {
+            mainSearch: {
+                category: action.category,
+                searchVal: action.searchVal,
+                searchResultDataList: action.searchResultDataList,
+            },
+        });
+    }
+
+    console.log("reducer()", state, action, newState);
+    return newState;
+}
+
+const enhancedReducer = persistReducer(persistConfig, reducer);
+
+export default createStore(enhancedReducer
     , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
