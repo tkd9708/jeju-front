1dfa2b646f1de28d117c14750f98033f82681d76
tkd9708 <tkd9708@naver.com>
2021-01-30 18:51:15 +0900

    myplan class로 변경

--- finalproject/src/component/main/mainpage/MyPlanComp.js
+++ finalproject/src/component/main/mainpage/MyPlanComp.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -11,163 +10,89 @@ import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import FlightLandIcon from '@material-ui/icons/FlightLand';
 import Divider from '@material-ui/core/Divider';
-
 import Paper from "@material-ui/core/Paper";
+import store from '../../../redux/store';
+import './MyPlanComp.css';
 
+class MyPlanComp extends Component {
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        // maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-    },
-    pagesRoot: {
-        display: "flex",
-        flexWrap: "wrap",
-        "& > *": {
-            margin: "10px",
-            padding: "10px",
-            width: "300px",
-            height: "400px",
-            overflow: "hidden",
-        }
-    }
-}));
+    render(){
+        const tag = store.getState().logged==true?
+            <div className="myPlanLeft">
+                    <h1>MyPlan</h1>
+                    <List style={{width: '100%'}}>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ImageIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Photos" secondary="Jan 9, 2014"/>
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <WorkIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Work" secondary="Jan 7, 2014"/>
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <BeachAccessIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" secondary="July 20, 2014"/>
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <EventAvailableIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" secondary="July 20, 2014"/>
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <FlightLandIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Vacation" secondary="July 20, 2014"/>
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                    </List>
+                </div>:"";
 
-export default function MyPlanComp() {
-    const classes = useStyles();
-    const styles = {
-        left: {
-            minWidth: "400px",
-            maxWidth: "400px",
-            height: "500px",
-            borderRight: "1px solid black",
-            padding: "20px",
-        },
-        right: {
-            flexGrow: "1",
-            height: "500px",
-            padding: "20px",
-            overflow: "scroll",
-            // borderRight: "1px solid black",
-        },
-        parent: {
-            display: "flex",
-        }
-    }
+        let date = new Date();
+        let today = date.getFullYear() + "-" + date.getMonth()+1 + "-" + date.getDate();
+        const myplanTitle = store.getState().logged==true?<h2>MyPlan on {today}</h2>:<h2>오늘의 제주 News</h2>
 
-    return (
-        <div className="myPlanComp" style={styles.parent}>
-            <div style={styles.left}>
-                <h1>MyPlan</h1>
-                <List className={classes.root}>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <ImageIcon/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary="Photos" secondary="Jan 9, 2014"/>
-                    </ListItem>
-                    <Divider variant="inset" component="li"/>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <WorkIcon/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary="Work" secondary="Jan 7, 2014"/>
-                    </ListItem>
-                    <Divider variant="inset" component="li"/>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <BeachAccessIcon/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                    </ListItem>
-                    <Divider variant="inset" component="li"/>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <EventAvailableIcon/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                    </ListItem>
-                    <Divider variant="inset" component="li"/>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <FlightLandIcon/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                    </ListItem>
-                    <Divider variant="inset" component="li"/>
-                </List>
-            </div>
-            <div style={styles.right}>
-                <h2>MyPlan on Jan 9, 2014</h2>
-                <div className={classes.pagesRoot}>
-                    <Paper elevation={3}>
-                        <List className={classes.root}>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <ImageIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary="Photos" secondary="Jan 9, 2014"/>
-                            </ListItem>
-                            <Divider variant="inset" component="li"/>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <WorkIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary="Work" secondary="Jan 7, 2014"/>
-                            </ListItem>
-                            <Divider variant="inset" component="li"/>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <BeachAccessIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                            </ListItem>
-                            <Divider variant="inset" component="li"/>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <EventAvailableIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                            </ListItem>
-                            <Divider variant="inset" component="li"/>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <FlightLandIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary="Vacation" secondary="July 20, 2014"/>
-                            </ListItem>
-                            <Divider variant="inset" component="li"/>
-                        </List>
-                    </Paper>
-                    <Paper elevation={3}></Paper>
-                    <Paper elevation={3}></Paper>
-                    <Paper elevation={3}></Paper>
-                    <Paper elevation={3}></Paper>
-                    <Paper elevation={3}></Paper>
-                    <Paper elevation={3}></Paper>
+        return (
+            <div className="myPlanComp myPlanParent">
+                {tag}
+                <div className="myPlanRight">
+                    {myplanTitle}
+                    <div className="myPlanpagesRoot">
+                        <Paper elevation={3}>
+                            
+                        </Paper>
+                        <Paper elevation={3}></Paper>
+                        <Paper elevation={3}></Paper>
+                        <Paper elevation={3}></Paper>
+                        <Paper elevation={3}></Paper>
+                        <Paper elevation={3}></Paper>
+                        <Paper elevation={3}></Paper>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default MyPlanComp;
