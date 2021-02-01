c2e60b194e50d761413a5fd2f4ecb5860caafc40
sykim86 <sykim8611@gmail.com>
2021-02-01 16:09:46 +0900

    어커밋합니다

--- finalproject/src/component/main/auth/GoogleLoginBtnComp.js
+++ finalproject/src/component/main/auth/GoogleLoginBtnComp.js
@@ -39,38 +39,37 @@ class GoogleLoginBtnComp extends Component
         });
     }
 
-    render() {
-        const responseGoogle = (response) => {
-            console.log(response);
-        }
-        
-        // 로그인에 성공하면 json을 반환해줌 => onSuccess함수에서 state에 id, name을 저장
-        const onSuccess = (res) => {
-            console.log('[Login Success] 로그인한 사용자:', res);
-            // console.log('로그인한 사용자 id : ' +  res.googleId);
+    responseGoogle = (response) => {
+        console.log(response);
+    }
+    
+    // 로그인에 성공하면 json을 반환해줌 => onSuccess함수에서 state에 id, name을 저장
+    onSuccess = (res) => {
+        console.log('[Login Success] 로그인한 사용자:', res);
+        // console.log('로그인한 사용자 id : ' +  res.googleId);
 
-        this.setState({
-            id: res.googleId,
-            name: res.profileObj.name,
-            email: res.profileObj.email,
-            email2: 'gmail.com',
-            photo: res.profileObj.imageUrl,
-        });
-        let url = URL + "/member/insertsosial";
+    this.setState({
+        id: res.googleId,
+        name: res.profileObj.name,
+        email: res.profileObj.email,
+        email2: 'gmail.com',
+        photo: res.profileObj.imageUrl,
+    });
+    let url = URL + "/member/insertsosial";
 
-        axios.post(url, {id:this.state.email, name:this.state.name, provider:this.state.id, 
-            photo:this.state.photo, email:this.state.email.split("@")[0], email2:this.state.email2})
-                .then(result=>{
+    axios.post(url, {id:this.state.email, name:this.state.name, provider:this.state.id, 
+        photo:this.state.photo, email:this.state.email.split("@")[0], email2:this.state.email2})
+            .then(result=>{
 
-                }).catch(err=>{
-                    console.log("google db 저장 실패 : " + err);
-                })
-        this.setGoogleOn();
+            }).catch(err=>{
+                console.log("google db 저장 실패 : " + err);
+            })
+    this.setGoogleOn();
 
-        alert("스토어에 저장된 구글로그인 상태 : " + store.getState().googleOn);
+    alert("스토어에 저장된 구글로그인 상태 : " + store.getState().googleOn);
 
-        // initializing the setup
-        refreshTokenSetup(res);
+    // initializing the setup
+    refreshTokenSetup(res);
     };
 
     // 로그인에 실패한 경우에는 에러를 넘겨줌 => onFailure함수에서 error 출력
@@ -79,8 +78,6 @@ class GoogleLoginBtnComp extends Component
     };
 
     render() {
-        
-
         return (
             <>
             <br />
