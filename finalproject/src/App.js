e9855396be2ad88c3075ced73c96dc31a24fa1d9
ykh <yangyk7364@gmail.com>
2021-02-01 14:47:32 +0900

    [MainComp] deleted .js .css

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -210,35 +210,4 @@ class App extends Component {
 }
 
 export default App;
-/*
-    HeaderComp
-        Title
-            Home
-            Notice
-            Reservation
-            Tour
-                TourList
-            ShareBoard
-            MyPage
-            Login / Logout
-    MainComp
-        Home
-            -
-            -
-        검색
-            - 검색 카테고리 select/option
-            - 단어검색어 input
-            - 검색 button
-        관광명소 - 지도 클릭
-            - 제주시 명소 링크.
-            - 애월읍 명소 링크.
-            ...
-        공지사항
-            - +버튼 -> 공지사항 페이지 이동. -button 안에 img
-            - 공지사항 리스트중 하나 클릭. -table td a tag 안에 span 문자열
-        공유게시판
-            - +버튼 -> 공유게시판 페이지 이동. -button 안에 img
-            - 공유게시판 리스트중 하나 클릭. -table td a tag 안에 span 문자열
-    footer
-        회사 정보
- */
+
