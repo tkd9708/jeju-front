67ba0cfbf5966f12b8e4a494b28e12a67cecd479
sykim86 <sykim8611@gmail.com>
2021-02-02 18:46:41 +0900

    MyPlanComp에 서 axios를 하려고했는데 걍 Weather.js에서 함ㅅ돠

--- finalproject/src/component/main/mainpage/Weather.js
+++ finalproject/src/component/main/mainpage/Weather.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 
 class Weather extends Component {
+
+    getWeatherList = () => {
+
+        let url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst';
+        let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + 'ijFCZNWcCKbWGchBc5vZ%2F%2FXIG5vnZeeOgt1m23u3U0BXhc8dVvq%2BdymzHUQDmarDgb0XcV%2BV7gmzgn9T3JSsZQ%3D%3D';
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10');
+        queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML');
+    }
     
     render(){
 
