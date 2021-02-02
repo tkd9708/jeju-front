351234e55ec78a9e5189b9fb3c4c2e6656e9b1cc
sykim86 <sykim8611@gmail.com>
2021-02-02 21:10:29 +0900

    동네예보조회로 url 데이터 받아오는걸로바꾸엇습니다

--- finalproject/src/component/main/mainpage/Weather.js
+++ finalproject/src/component/main/mainpage/Weather.js
@@ -5,7 +5,7 @@ class Weather extends Component {
 
     getWeatherList = () => {
 
-        let url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getCityTourClmIdx';
+        let url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst';
         let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + 'ijFCZNWcCKbWGchBc5vZ%2F%2FXIG5vnZeeOgt1m23u3U0BXhc8dVvq%2BdymzHUQDmarDgb0XcV%2BV7gmzgn9T3JSsZQ%3D%3D';
         queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
         queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10');
@@ -23,8 +23,9 @@ class Weather extends Component {
         hours = hours < 10 ? '0' + hours : hours;
         // document.write(year+month+date+hours);
         queryParams += '&' + encodeURIComponent('CURRENT_DATE') + '=' + encodeURIComponent(year+month+date+hours);
-        queryParams += '&' + encodeURIComponent('DAY') + '=' + encodeURIComponent('8'); // CURRENT_DATE부터 7일후 (총8일)까지의 자료 호출
-        queryParams += '&' + encodeURIComponent('CITY_AREA_ID') + '=' + encodeURIComponent('5013000000'); // 시군구 아이디
+        let callHour = String(24 * 8); // 8일
+        queryParams += '&' + encodeURIComponent('HOUR') + '=' + encodeURIComponent(callHour); // CURRENT_DATE부터 8일 후까지의 자료 호출        
+        queryParams += '&' + encodeURIComponent('COURSE_ID') + '=' + encodeURIComponent('1'); // 관광 코스ID
         
         url = url + queryParams;
 
