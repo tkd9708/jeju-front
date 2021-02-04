1ff2f53ed5d608b6eb00def365e659b688c495c4
yanghojun <twinhj@naver.com>
2021-02-04 21:07:32 +0900

    reservation 작업중

--- finalproject/src/component/main/mainpage/Weather.js
+++ finalproject/src/component/main/mainpage/Weather.js
@@ -19,22 +19,19 @@ class Weather extends Component {
         console.log("Weather class 생성자", props);
 
         // 리덕스스토어에구독한다
-<<<<<<< HEAD
+
 
         // store.subscribe(function() {
         //     console.log("날씨 클래스 생성자에서 state 변경에 대한 변화를 구독합니다 변화를 확인했습니다 store에서 weatherInfo 값을 가져와 첫번째 courseAreaName을 보여줍니다 : " + store.getState().weatherInfo[0].courseName);
         // }.bind(this));
 
-=======
->>>>>>> 1ed3a92a9efe27aad0538559a000ae61a315f017
+
         store.subscribe(function() {
 
             // console.log("날씨 클래스 생성자에서 state 변경에 대한 변화를 구독합니다 변화를 확인했습니다 store에서 weatherInfo 값을 가져와 첫번째 courseAreaName을 보여줍니다 : " + store.getState().weatherInfo[0].courseName);
         }.bind(this));
-<<<<<<< HEAD
 
-=======
->>>>>>> 1ed3a92a9efe27aad0538559a000ae61a315f017
+
         // 리덕스스토어에구독한다
 
         // 리덕스를 안쓰고 클래스 내부 state를 씁니다
