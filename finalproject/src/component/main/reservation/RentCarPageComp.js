5d6484dde10803d2ea0debe9aa953b6bfa02a526
redhorn64 <reliable0524@gmail.com>
2021-02-04 14:06:42 +0900

    렌트카 예약저장 작업중

--- finalproject/src/component/main/reservation/RentCarPageComp.js
+++ finalproject/src/component/main/reservation/RentCarPageComp.js
@@ -1,22 +1,22 @@
 import React, {Component} from "react";
+import {URL} from '../../../redux/config';
+import RentCarItem from "./RentCarItem";
 import axios from 'axios';
-import {URL} from "../../../redux/config";
-import RentCarItem from './RentCarItem';
 
 class RentCarPageComp extends Component {
-    state={
-        listData:[] 
-    }
     constructor(props) {
         super(props);
         console.log("RentCarPageComp constructor", props);
+        this.state={
+            listData:[]
+        };
     }
 
     list=()=>{
         let url= URL + "/reservation/carlist";
         console.log(url);
 
-        axios.post(url)
+        axios.get(url)
         .then(res=>{
             this.setState({
                 listData:res.data
@@ -24,31 +24,37 @@ class RentCarPageComp extends Component {
         })
     }
 
+    componentWillMount()
+    {
+        this.list();
+    }
+
     render() {
         return (
             <div>
-                <table style={{width:'900px'}} id="rentcarTable">
-                    <caption><b>렌트카 업체명</b></caption>
+                <b>렌트카 업체 목록</b>
+                <table className="table table-bordered" style={{width:'1200px'}}>
                     <thead>
                         <tr>
-                            <th style={{width:'50px'}}>번호</th>
-                            <th style={{width:'200px'}}>업체명</th>
-                            <th style={{width:'100px'}}>사업장</th>
-                            <th style={{width:'300px'}}>주소</th>
-                            <th style={{width:'80px'}}>차량수</th>
-                            <th style={{width:'100px'}}>시작시간</th>
-                            <th style={{width:'100px'}}>종료시간</th>
-                            <th style={{width:'200px'}}>홈페이지</th>
-                            <th style={{width:'100px'}}>전화번호</th>
-                            <th style={{width:'100px'}}>확인일</th>
+                            <td style={{width:'50px'}}>#</td>
+                            <td style={{width:'200px'}}>업체명</td>
+                            <td style={{width:'100px'}}>구분</td>
+                            <td style={{width:'200px'}}>주소</td>
+                            <td style={{width:'80px'}}>차량대수</td>
+                            <td style={{width:'80px'}}>시작시간</td>
+                            <td style={{width:'80px'}}>종료시간</td>
+                            <td style={{width:'130px'}}>홈페이지</td>
+                            <td style={{width:'150px'}}>전화번호</td>
+                            <td style={{width:'150px'}}>확인일</td>
+                            <td style={{width:'150px'}}>일정추가</td>
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                      this.state.listData.map((row,num)=>(
-                        <RentCarItem row={row} key={num} num={num}/>
-                        ))
-                    }
+                        {
+                            this.state.listData.map((row, num)=>(
+                                <RentCarItem row={row} key={num} num={num}/>
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>
