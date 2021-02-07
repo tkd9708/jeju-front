0d76d7b956263dc28ecadf77ca1754b1dd0c67cd
tkd9708 <tkd9708@naver.com>
2021-02-08 02:00:53 +0900

    notice 스마트에디터 추가

--- finalproject/src/component/main/notice/BoardItem.js
+++ finalproject/src/component/main/notice/BoardItem.js
@@ -2,34 +2,40 @@ import React,{Component} from 'react';
 import {Route, Link} from "react-router-dom";
 import axios from "axios";
 import {URL} from '../../../redux/config';
+import store from "../../../redux/store";
+import moment from 'moment';
 
 class BoardItem extends Component
 {
     updateStar=()=>{
         let url = URL + "/notice/updatestar?num=" + this.props.row.num;
 
-        axios.get(url)
-        .then(res=>{
-            this.props.list();
-        }).catch(err=>{
-            console.log("notice updateStar 오류 : " + err)
-        })
+        if(store.getState().loginId==='admin'){
+            axios.get(url)
+            .then(res=>{
+                this.props.list();
+            }).catch(err=>{
+                console.log("notice updateStar 오류 : " + err)
+            })
+        }
+        
     }
 
     render(){
         const {row, idx, history}=this.props;
+        let color = row.star == 1?"#fafafa":"";
+        const day = moment(row.writeday).format('YYYY-MM-DD');
         return(
             
-            <tr>
-
-                <td align='center'>{idx+1}</td>
+            <tr style={{backgroundColor: color}}>
+                <td align='center'>{idx}</td>
                 
-                    <td align='center'>
+                    <td>
                         
                         <Link to={`/notice/content/${row.num}`}>{row.subject}</Link>
                     </td>
-                <td align='center'>{row.writeday.toLocaleString('ko-KR')}</td>
-                <td align='left'>{row.star==1?<span style={{cursor: 'pointer'}} onClick={this.updateStar.bind(this)}>⭐</span>
+                <td align='center'>{day}</td>
+                <td align='center'>{row.star==1?<span style={{cursor: 'pointer'}} onClick={this.updateStar.bind(this)}>⭐</span>
                     :<span className="far fa-star" style={{cursor: 'pointer'}} onClick={this.updateStar.bind(this)}></span>}</td>
 
             </tr>
--- finalproject/src/component/main/notice/NoticeContent.js
+++ finalproject/src/component/main/notice/NoticeContent.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React,{Component}  from 'react';
 import {URL} from '../../../redux/config';
+import './NoticeCss.css';
+import store from "../../../redux/store";
 
 class NoticeContent extends Component
 {
@@ -33,7 +35,7 @@ class NoticeContent extends Component
         if(window.confirm("삭제하시겠습니까?")){
             axios.get(url)
             .then(res=>{
-                this.props.history.push("/notice");
+                this.props.history.push("/notice/1");
             }).catch(err=>{
                 console.log("notice delete 오류 : " + err);
             })
@@ -51,34 +53,57 @@ class NoticeContent extends Component
 
 
         return (
-            <div>
-                <table className="table table-bordered"
-                style={{width:'400px'}}>
-                    <caption><h3>{selectData.title}</h3></caption>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <b>작성자:{selectData.subject}</b>
-                                &nbsp;&nbsp;
-                               <pre>내용:{selectData.content}</pre>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2" align='center'>
-                                <button type="button" className="btn btn-success btn-sm" style={{marginLeft:'100px',width:'100px'}}
-                                    onClick={
-                                        ()=>{
-                                            this.props.history.push("/notice/update/" + this.num);
-                                        }
-                                    }>수정</button>
-                                <button type="button" className="btn btn-success btn-sm" style={{marginLeft:'100px',width:'100px'}}
-                                    onClick={this.onDeleteData.bind(this)}>삭제</button>
+            <div className="noticePageDiv">
+                <div className="detailTitle">
+                    <span className="detailTitleContent" style={{backgroundColor: 'white', color: '#036E38'}}>
+                        &nbsp;&nbsp;공지사항&nbsp;&nbsp;
+                    </span>
+                </div>
+                <div className="detailIntro" style={{color: "#888"}}>
+                    관광지나 업체, 제주도 축제 등 중요한 소식을 알려드립니다.<br/>
+                </div>
+                <br/>
+                <div className="noticeTable" style={{margin: '0 auto'}}>
+                    {store.getState().loginId=='admin'?
+                    <div style={{float: 'right'}}>
+                         <a href={`/notice/update/${this.num}`} style={{color: 'gray'}}>수정</a>&nbsp;/&nbsp;
+                        <span style={{color: 'gray', cursor: 'pointer'}} onClick={this.onDeleteData.bind(this)}>삭제</span>
+                    </div>:""    
+                    }
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <td align="center">
+                                    <b>{selectData.subject}</b>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div dangerouslySetInnerHTML={ {__html: selectData.content} } className="noticeContentEditor"></div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align='center'>
+                                    <button type="button" class="btn btn-dark-green" onClick={()=>{
+                                        this.props.history.goBack();
+                                    }}>목록</button>
+                                    {/* <button type="button" className="btn btn-success btn-sm" style={{marginLeft:'100px',width:'100px'}}
+                                        onClick={
+                                            ()=>{
+                                                this.props.history.push("/notice/update/" + this.num);
+                                            }
+                                        }>수정</button>
+                                    <button type="button" className="btn btn-success btn-sm" style={{marginLeft:'100px',width:'100px'}}
+                                        onClick={this.onDeleteData.bind(this)}>삭제</button> */}
 
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
             </div>
         )
     }
