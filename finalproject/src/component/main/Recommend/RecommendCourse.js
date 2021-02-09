b1c418c76d19324c8ee1138baef7b976244fcf55
tkd9708 <tkd9708@naver.com>
2021-02-09 13:44:04 +0900

    recommend 추가

---
+++ finalproject/src/component/main/Recommend/RecommendCourse.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import store from '../../../redux/store';
+import axios from 'axios';
+
+
+class RecommendCourse extends Component{
+    
+    render(){
+        
+
+        return(
+            
+                <div>
+                    <h4>RecommendCourse</h4>
+                </div>
+            
+            
+        )
+    }
+}
+
+export default RecommendCourse;
